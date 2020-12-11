ec9fb8e0bc458fa7892c16c6f13fb9701bab9f00
AKArow <abhishek.kulkarni.1999@gmail.com>
2020-12-11 20:49:48 +0530

    Total Wages With Days

--- dailytotalwage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-dw=160
-tw=$dw
-echo "Daily Wages:" $dw
-echo "Total Wages:" $tw
---
+++ daydailytotal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+d=3
+dw=160
+tw=$(($d*$dw))
+echo "Employee Working Day:" $d
+echo "Employee Daily Wages:" $dw
+echo "Employee Total Wages:" $tw

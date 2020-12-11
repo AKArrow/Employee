2f402e3c04abf8ccda6e0ed13fa45345b6cbb521
AKArow <abhishek.kulkarni.1999@gmail.com>
2020-12-11 20:41:13 +0530

    Total And Daily Wages

---
+++ dailytotalwage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+dw=160
+tw=$dw
+echo "Daily Wages:" $dw
+echo "Total Wages:" $tw
--- workhour.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash/ -x
-hour()
-{
-	echo "Employee Working Hours:" $1
-}
-echo "Enter Employee Working Hour:"
-read n
-hour $n
